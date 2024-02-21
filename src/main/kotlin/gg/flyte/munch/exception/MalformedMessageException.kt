653ea8cb605981762bcafc27eb2d1692a9603851
Josh <43449531+joshbker@users.noreply.github.com>
2024-02-21 13:28:03 +0000

    feat: use base MunchException

--- src/main/kotlin/gg/flyte/munch/exception/MalformedMessageException.kt
+++ src/main/kotlin/gg/flyte/munch/exception/MalformedMessageException.kt
@@ -1,3 +1,3 @@
 package gg.flyte.munch.exception
 
-class MalformedMessageException : IllegalArgumentException("Messages must have a defined sender uid.")
+class MalformedMessageException : MunchException("Messages must have a defined sender uid.")
---
+++ src/main/kotlin/gg/flyte/munch/exception/MunchException.kt
@@ -0,0 +1,3 @@
+package gg.flyte.munch.exception
+
+open class MunchException(message: String) : Exception(message)
