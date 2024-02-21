832aed8076fead257723969f0209586796cdb94a
Josh <43449531+joshbker@users.noreply.github.com>
2024-02-21 13:38:52 +0000

    feat: improve format/readability of DefaultMessageHandler

--- src/main/kotlin/gg/flyte/munch/message/DefaultMessageHandler.kt
+++ src/main/kotlin/gg/flyte/munch/message/DefaultMessageHandler.kt
@@ -3,8 +3,6 @@ package gg.flyte.munch.message
 import gg.flyte.munch.Munch
 import gg.flyte.munch.Munch.Companion.log
 import gg.flyte.munch.exception.MalformedMessageException
-import gg.flyte.munch.exception.UnknownServerException
-import gg.flyte.munch.server.Server
 import gg.flyte.munch.server.ServerRegistry
 import java.util.*
 import java.util.concurrent.Executors
@@ -26,32 +24,35 @@ class DefaultMessageHandler(
         if (message.uid in handledMessages) return
 
         handledMessages += message.uid
+        
         cleanupService.schedule({
             munch.clean(message.uid.toString())
             handledMessages -= message.uid
         }, munch.messageLifetime, TimeUnit.MILLISECONDS)
 
         val sender = message.sender ?: throw MalformedMessageException()
+
         if (sender == munch.server.id) return
         if (!message.isGlobal() && munch.server.id !in message.destinations) return
 
         when (message.header) {
-            Message.Header.MUNCH_HANDSHAKE_CONNECT -> {
-                val server = ServerRegistry.register(id = sender, name = message.content)
-                log("[DefaultMessageHandler - MUNCH_HANDSHAKE_CONNECT] Discovered new Muncher: $server")
+
+            Message.Header.MUNCH_HANDSHAKE_CONNECT -> with(ServerRegistry.register(sender, message.content)) {
+                log("[DefaultMessageHandler - MUNCH_HANDSHAKE_CONNECT] Discovered new Muncher: $this")
+
                 munch.message {
-                    destinations = setOf(server.id)
+                    destinations = setOf(id)
                     header = Message.Header.MUNCH_HANDSHAKE_CONFIRM
                     content = munch.server.name
                 }
             }
 
-            Message.Header.MUNCH_HANDSHAKE_CONFIRM -> {
-                val server = ServerRegistry.register(id = sender, name = message.content)
-                log("[DefaultMessageHandler - MUNCH_HANDSHAKE_CONFIRM] Discovered new Muncher: $server")
+            Message.Header.MUNCH_HANDSHAKE_CONFIRM -> ServerRegistry.register(sender, message.content).also {
+                log("[DefaultMessageHandler - MUNCH_HANDSHAKE_CONFIRM] Discovered new Muncher: $this")
             }
 
             else -> providedHandler.handle(message)
+
         }
     }
 }
