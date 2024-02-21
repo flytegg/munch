f5a34763ee12db3a92ffee13981d1c2602bbb434
Josh <43449531+joshbker@users.noreply.github.com>
2024-02-21 13:31:55 +0000

    feat: add register, unregister, findById, findByName, values

--- src/main/kotlin/gg/flyte/munch/server/ServerRegistry.kt
+++ src/main/kotlin/gg/flyte/munch/server/ServerRegistry.kt
@@ -3,7 +3,15 @@ package gg.flyte.munch.server
 import java.util.*
 
 object ServerRegistry {
-    val servers = mutableMapOf<UUID, Server>()
+    private val servers = mutableMapOf<UUID, Server>()
 
-    fun findServerByUid(uid: UUID): Server? = servers[uid]
+    fun register(id: UUID, name: String) = servers.put(id, Server(id, name))
+
+    fun unregister(id: UUID) = servers.remove(id)
+
+    fun findById(id: UUID): Server? = servers[id]
+
+    fun findByName(name: String): Server? = servers.values.find { it.name == name }
+
+    fun values(): Collection<Server> = servers.values
 }
