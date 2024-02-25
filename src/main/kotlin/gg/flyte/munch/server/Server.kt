36092a4ce059b5fb7739d926b8ffe6486b688b52
Josh <43449531+joshbker@users.noreply.github.com>
2024-02-25 17:11:57 +0000

    feat: nvm

--- src/main/kotlin/gg/flyte/munch/server/Server.kt
+++ src/main/kotlin/gg/flyte/munch/server/Server.kt
@@ -2,7 +2,7 @@ package gg.flyte.munch.server
 
 import java.util.*
 
-open class Server(
+class Server(
     val id: UUID,
     var name: String = "Unknown server",
     var lastKeepAlive: Long = -1L
